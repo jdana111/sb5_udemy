b586e8d13d23f42fe07d0d5c6f81726768dcc4af
John Dana <john@danasd.com>
2023-03-05 11:43:22 -0700

    02 16 Practice with "JavaScript space" in JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-  <div>
-    <label htmlFor="name" className="label">
-      enter email address
-    </label>
-    <input id="name" type="text" />
-    <button style={{ backgroundColor: "red", color: "white" }}>Submit</button>
-  </div>
-);
+const App = () => {
+  const buttonText = { text: "Click Me"}
+  return (
+    <div>
+      <label htmlFor="name" className="label">
+        enter email address
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "red", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
